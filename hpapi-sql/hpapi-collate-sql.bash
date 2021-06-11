199ebc50bb89ec3ff5f96ab692c363cd5d080cf8
Mark Page <mark.page@burdenandburden.co.uk>
2021-06-11 20:22:28 +0000

    Improves collation of SQL

--- hpapi-sql/hpapi-collate-sql.bash
+++ hpapi-sql/hpapi-collate-sql.bash
@@ -4,23 +4,52 @@
 #   bash ~/hpapi/whitelamp-uk/hpapi-sql/hpapi-collate-sql.bash ~/hpapi/some-vendor/some-sql
 # To collate the entire initialisation SQL for the entire Hpapi system
 #   bash ~/hpapi/whitelamp-uk/hpapi-sql/hpapi-collate-sql.bash ~/hpapi
+# If you just want the file paths, use -q as the first argument
+if [ "$1" = "-q" ]
+then
+    quiet="1"
+    shift
+fi
 if [ "$1" ]
 then
     if [ ! -d "$1" ]
     then
         echo "'$1' is not a directory"
-        exit;
+        exit 101;
     fi
-    dir="$(realpath $1)"
+    dirs="$(realpath $1)"
 else
-    dir="$(dirname $(realpath "$0"))"
+    dirs="$(dirname $(realpath "$0"))"
 fi
-echo -e "\n\n-- ############ $dir COLLATED SQL ############\n\n"
-find "$dir" -iname 'tables.sql' -print0         |  while IFS= read -r -d '' line
-do echo -e "\n\n-- ############ $line ############" ; cat "$line" ; done
-find "$dir" -iname 'rows.sql' -print0           |  while IFS= read -r -d '' line
-do echo -e "\n\n-- ############ $line ############" ;  cat "$line" ; done
-find "$dir" -iname 'functions.sql.sql' -print0  |  while IFS= read -r -d '' line
-do echo -e "\n\n-- ############ $line ############" ;  cat "$line" ; done
-find "$dir" -iname 'routines.sql' -print0       |  while IFS= read -r -d '' line
-do echo -e "\n\n-- ############ $line ############" ;  cat "$line" ; done
+if [ "$2" ]
+then
+    if [ ! "$quiet" ]
+    then
+        echo -e "\n-- ############ COLLATED SQL MODEL: $2 ############\n"
+    fi
+    dirs="$(find $dirs -type d -iname $2)"
+fi
+
+for type in tables.sql rows.sql functions.sql routines.sql
+do
+    if [ ! "$quiet" ]
+    then
+        echo -e "\n-- ############ COLLATED SQL: $2 $type ############\n"
+    fi
+    for dir in $dirs
+    do
+        for file in $(find -L "$dir" -type f -iname $type)
+        do
+            if [ "$quiet" ]
+            then
+                echo $file
+                continue
+            fi
+            if [ ! "$quiet" ]
+            then
+                echo -e "\n-- ############ $2 $type $file ############\n" ;
+            fi
+            cat "$file"
+        done
+    done
+done
