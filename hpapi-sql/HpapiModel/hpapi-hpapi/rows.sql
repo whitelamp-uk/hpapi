6e3b275184a7cda5e9cb1cab7bad8d58694407d9
Mark Page <mark.page@burdenandburden.co.uk>
2022-01-31 10:53:35 +0000

    Improves usergroup notes, adds a public (but not anon) usergroup to the default model

--- hpapi-sql/HpapiModel/hpapi-hpapi/rows.sql
+++ hpapi-sql/HpapiModel/hpapi-hpapi/rows.sql
@@ -70,10 +70,11 @@ INSERT IGNORE INTO `hpapi_pattern` (`pattern`, `constraints`, `expression`, `inp
 INSERT IGNORE INTO `hpapi_usergroup` (`usergroup`, `level`, `name`, `password_self_manage`, `remote_addr_pattern`, `notes`) VALUES
 ('system',	1,	'System',	0,	'^::1$',	'Automated system routines.'),
 ('admin',	2,	'Administrators',	0,	'^.*$',	'Users performing high level administration of business data within the model.'),
-('agent',	10000,	'Client Agents',	1,	'^.*$',	'Custom user group for users acting on behalf of a company client. They may or may not be operating via a third party agency organisation (eg. a lottery provider).'),
-('anon',	10000000,	'Unknown Users',	1,	'^.*$',	'Users having no identity.'),
-('field',	1000,	'Field staff',	1,	'^.*$',	'Staff at off-site locations'),
 ('manager',	10,	'Managers',	'^.*$',	0,	'Admin users with relatively high privileges'),
 ('staff',	100,	'Office staff',	'^.*$',	0,	'Admin users with limited privileges'),
+('field',	1000,	'Field staff',	1,	'^.*$',	'Staff at off-site locations'),
+('agent',	10000,	'Client Agents',	1,	'^.*$',	'Users acting on behalf of a company client. They may or may not be operating via a third party agency organisation.'),
+('public',	100000,	'Public users',	1,	'^.*$',	'Public unaffiliated registered users'),
+('anon',	10000000,	'Unknown Users',	1,	'^.*$',	'Users having no identity.'),
 ('sysadmin',	3,	'System Administrators',	0,	'^.*$',	'Users performing high level administration of configuration data within the model.');
 
